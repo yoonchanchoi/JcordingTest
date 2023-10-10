65bb766486c45fbf6fb2c4aa922b19bd7aed6403
Choi YoonChan <choyc9239@gmail.com>
2023-10-10 15:40:21 +0900

    Initial commit

---
+++ app/src/main/java/com/example/jcordingtest/추억점수/Solution.java
@@ -0,0 +1,12 @@
+package com.example.jcordingtest.추억점수;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = {};
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+    }
+}

dd8fc85d216fa510be66488f7f1b31cbf0e90cfd
Choi YoonChan <choyc9239@gmail.com>
2023-10-10 18:39:21 +0900

    코테 추억 점수

--- app/src/main/java/com/example/jcordingtest/MainActivity.java
+++ app/src/main/java/com/example/jcordingtest/MainActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.jcordingtest.추억점수.Solution;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    public void init(){
+        Solution st = new Solution();
+        st.main();
     }
 }
--- app/src/main/java/com/example/jcordingtest/추억점수/Solution.java
+++ app/src/main/java/com/example/jcordingtest/추억점수/Solution.java
@@ -1,12 +1,40 @@
 package com.example.jcordingtest.추억점수;
 
-class Solution {
+import android.util.Log;
+
+public class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
-        int[] answer = {};
+        Log.e("cyc","photo.length--->"+photo.length);
+        int[] answer = new int[photo.length];
+
+        for(int i =0; i<photo.length; i++){
+                Log.e("cyc","");
+                int temp=0;
+            for(int j=0; j<photo[i].length;j++){
+                for(int k =0; k<name.length;k++){
+                    if(name[k].equals(photo[i][j])){
+                        temp+=yearning[k];
+                    }
+                }
+                Log.e("cyc",String.format("photo[%d][%d]-->%s",i,j,photo[i][j]));
+            }
+            answer[i]=temp;
+        }
+
+
+        for(int i = 0 ;i< answer.length;i++){
+            Log.e("cyc",String.format("answer[%d]-->%d",i,answer[i]));
+
+        }
+
 
         return answer;
     }
 
-    public static void main(String[] args) {
+    public void main() {
+        String[] name ={"may", "kein", "kain", "radi"};
+        int[] yearning = {5, 10, 1, 3};
+        String[][] photo = {{"may", "kein", "kain", "radi"},{"may", "kein", "brin", "deny"}, {"kon", "kain", "may", "coni"}};
+        solution(name, yearning, photo);
     }
 }

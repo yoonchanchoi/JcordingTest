aa41285cb953763bc30aae6662406f03f61b0629
Choi YoonChan <choyc9239@gmail.com>
2023-10-19 21:21:23 +0900

    코테 덧칠하기

---
+++ app/src/main/java/com/example/jcordingtest/덧칠하기/Solution6.java
@@ -0,0 +1,27 @@
+package com.example.jcordingtest.덧칠하기;
+
+public class Solution6 {
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        return answer;
+    }
+
+
+//    public int solution2(int n, int m, int[] section) {
+//        int start = section[0];
+//        int end = section[0] + (m-1);
+//        int ans = 1;
+//
+//        for(int i : section){
+//            if(i>=start && i<=end)
+//                continue;
+//            else{
+//                start = i;
+//                end = i + (m-1);
+//                ans++;
+//            }
+//
+//        }
+//        return ans;
+//    }
+}

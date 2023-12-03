c88eb27c972692a694c161dd6c7a000e0009a8b3
Choi YoonChan <choyc9239@gmail.com>
2023-12-03 17:39:09 +0900

    코테 Solution47 피로도 Solution48 [1차]뉴스클러스터랑

---
+++ app/src/main/java/com/example/jcordingtest/단계2/피로도/Solution47.java
@@ -0,0 +1,25 @@
+package com.example.jcordingtest.단계2.피로도;
+
+import java.util.*;
+
+public class Solution47 {
+    static boolean[] visited;static int count = 0;
+
+    public int solution(int k, int[][] dungeons) {
+        visited = new boolean[dungeons.length];
+        dfs(0, k, dungeons);
+        return count;
+    }
+
+    private void dfs(int depth, int fatigue, int[][] dungeons) {
+        for (int i = 0; i < dungeons.length; i++) {
+            if (visited[i] || dungeons[i][0] > fatigue) {
+                continue;
+            }
+            visited[i] = true;
+            dfs(depth + 1, fatigue - dungeons[i][1], dungeons);
+            visited[i] = false;
+        }
+        count = Math.max(count, depth);
+    }
+}

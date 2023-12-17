4247f28daeef7383380f0514f6d03c0bf0fb7d8a
Choi YoonChan <choyc9239@gmail.com>
2023-12-17 23:56:01 +0900

    코테 Solution56 더 맵게

---
+++ app/src/main/java/com/example/jcordingtest/단계2/더맵게/Solution56.java
@@ -0,0 +1,21 @@
+package com.example.jcordingtest.단계2.더맵게;
+
+import java.util.*;
+
+public class Solution56 {
+        public int solution(int[] scoville, int K) {
+            int answer = 0;
+            PriorityQueue<Integer> que = new PriorityQueue<>();
+
+            for (int i = 0; i < scoville.length; i++) {
+                que.add(scoville[i]);
+            }
+            while(que.peek() < K) {
+                if (que.size() == 1)
+                    return -1;
+                que.add(que.poll() + que.poll() * 2);
+                answer++;
+            }
+            return answer;
+        }
+}
